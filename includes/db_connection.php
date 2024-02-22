247dcf6ac2e12a6c49e977cfd2387d8163303d59
Harshada Dafare <rohandafare+github@gmail.com>
2024-02-22 20:43:01 +0530

    conneted database

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,1 +1,12 @@
- 
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = "";
+$dbname = "ut_cinema"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
