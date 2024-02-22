3687d9f576d92cad932dce552d11a9b0961d214a
Harshada Dafare <rohandafare+github@gmail.com>
2024-02-22 21:00:34 +0530

    added styles for login page

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Admin Login</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="container">
